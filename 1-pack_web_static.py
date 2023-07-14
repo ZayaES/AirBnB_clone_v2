c3bb23cbeaaa6d55bb334335eec92181efd578e3
Zaya Bell <heavenphrince@gmail.com>
2023-07-14 22:48:24 +0000

    task1 test1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" a fab script to generate an archive on local"""
+from datetime import datetime
+from fabric.api import local
+def do_pack():
+	""" packs a folder in archive"""
+
+	dt = datetime.now()
+	new_file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+							dt.month,
+							dt.day,
+							dt.hour,
+							dt.minute,
+							dt.second)
+
+	result = local("mkdir -p versions")
+	if (result.failed == True):
+		return None
+	archive = local("tar -cvzf {} web_static".format(new_file))
+	if (archive.failed == True):
+		return None
+	return new_file
