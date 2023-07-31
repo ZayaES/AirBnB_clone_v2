a1f5a52a9be5eb09db4955624c468c252f5f5258
Zaya Bell <heavenphrince@gmail.com>
2023-07-31 22:03:36 +0000

    task5 test1

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""A flask web application. Still don't know what I'm doing"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>')
+def display_text(text):
+    form_text = text.replace('_', ' ')
+    return "C " + form_text
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text=None):
+    if text is None:
+        text="is cool"
+    form_text = text.replace('_', ' ')
+    return "Python " + form_text
+
+@app.route('/number/<int:n>')
+def number(n):
+    return "{} is a number".format(n)
+
+@app.route('/number_template/<int:n>')
+def n_temp(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
