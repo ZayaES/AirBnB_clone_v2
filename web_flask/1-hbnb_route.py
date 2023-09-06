b1f2a482dc102604668567104d763706ce1e9d81
Zaya <heavenphrince@gmail.com>
2023-09-06 06:25:32 +0000

    shouldve been done a long time ago cgd2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,10 +13,12 @@ def hello():
     """ returns hello"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb')
 def hbnb():
     """returns hbnb"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
